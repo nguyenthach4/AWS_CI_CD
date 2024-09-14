a610382ef7eeafaf8883b670134d6619555588f0
nguyenthach4 <nguyenthachcitb1606917@gmail.com>
2024-09-14 23:00:36 +0700

    init terraform

--- setup/terraform/versions.tf
+++ setup/terraform/versions.tf
@@ -3,10 +3,11 @@ provider "aws" {
 }
 
 terraform {
+  required_version = "1.4.5"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.55"
+      version = "4.55.0"
     }
   }
 }
